7b9f6bb53e46f4ce8b86050ea67feb0c4d8b5d0c
btrinh1 <btrinh@ualberta.ca>
2015-01-28 14:02:35 -0700

    changed type of birthdate to date

--- server/cgi/2.py
+++ server/cgi/2.py
@@ -13,7 +13,7 @@ print """Content-Type: text/html
 <form method="get" action="1.py">
 name: <input type="text" name="name" />
 <div> family: <input type="text" name="family" /> </div>
-<div> birthdate: <input type="text" name="birthdate" /> </div>
+<div> birthdate: <input type="date" name="birthdate" /> </div>
 <diuv> main hobby: <input type="text" name="main hobby" /> </div>
 <div> <input type="submit" value="Send"> </div>
 </form>
